83aecf9868d75e1b04223723bb32c1beb06d8792
Rasel Miah <rasel6041@gmail.com>
2022-01-27 09:16:14 +0100

    mapping, creating object

--- learning-linkdin/src/App.js
+++ learning-linkdin/src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import biff from './biff.jpg'
+
+export function Header(props){
+  console.log(props)
+  return (
+    <header>
+       <h1>{ props.name }</h1>
+    </header>
+   
+  )
+}
+
+export function Main(props){
+  return (
+    <section>
+      <p>Here is our all menu</p>
+      <img src={ biff } alt="biff-images" height={200}/>
+      <ul style={{textAlign: 'left'}}>
+        {props.menu.map((menu)=><li  key={menu.id} >{ menu.title }</li>)}
+      </ul>
+    </section>
+  )
+}
+
+export function Footer(props){
+  return (
+    <section>
+      <p> Copyright {props.year}</p>
+    </section>
+  )
+}
+
+
+const menu=[
+  'Burger',
+  'Biff',
+  'Pasta'
+];
+
+
+const menuAsObj=menu.map((menu, i)=>({id:i, title: menu}))
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header name="Deli Bar&Kök "/>
+      <Main menu={menuAsObj}/>
+      <Footer year={new Date().getFullYear()}/>
     </div>
   );
 }
