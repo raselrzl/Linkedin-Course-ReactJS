10060acf219b164b357a57d9f7b83a43d9304673
Rasel Miah <rasel6041@gmail.com>
2022-01-29 07:14:20 +0100

    useEffect basic

--- learning-linkdin/src/App.js
+++ learning-linkdin/src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App(props) {
   const [name, setName]=useState('Rasel')
 
 
+  useEffect(()=>{
+      console.log(`It's ${name} !`)
+  }, [name]);
+  
+  useEffect(()=>{
+
+  },[])
 
  return ( <div>
               <h1>Hello {name}</h1>
