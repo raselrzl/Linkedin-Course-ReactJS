512df996b135968a92625eff0d8590cd3edc0755
Rasel Miah <rasel6041@gmail.com>
2022-01-29 07:23:10 +0100

    useReducer

--- learning-linkdin/src/App.js
+++ learning-linkdin/src/App.js
@@ -1,23 +1,21 @@
 import './App.css';
-import React, { useState, useEffect } from 'react';
+import React, { useReducer } from 'react';
 
-function App(props) {
-  const [name, setName]=useState('Rasel')
+function App() {
+  const [checked, toggle]=useReducer(
+    (checked)=>!checked,
+    false
+  )
 
-
-  useEffect(()=>{
-      console.log(`It's ${name} !`)
-  }, [name]);
-  
-  useEffect(()=>{
-
-  },[])
-
- return ( <div>
-              <h1>Hello {name}</h1>
-              <button onClick={()=>setName('Zeras')}>ClickMe</button>
+ return (
+          <div>
+              <input 
+                type="checkbox"
+                value={checked}
+                onChange={toggle}
+              />
           </div>
- );
+ )
 }
 
 export default App;
