9b719ceb5bf5d7120d4370441c0d918fedf65343
Rasel Miah <rasel6041@gmail.com>
2022-01-29 04:57:30 +0100

    conditional rendering

--- learning-linkdin/src/App.js
+++ learning-linkdin/src/App.js
@@ -1,57 +1,21 @@
 import './App.css';
 import React from 'react';
-import biff from './biff.jpg'
 
-export function Header(props){
-  console.log(props)
-  return (
-    <header>
-       <h1>{ props.name }</h1>
-    </header>
-   
-  )
+
+export function SecretComponent(){
+  return <h1>Secret Component</h1>
 }
 
-export function Main(props){
-  return (
-    <section>
-      <p>Here is our all menu</p>
-      <img src={ biff } alt="biff-images" height={200}/>
-      <ul style={{textAlign: 'left'}}>
-        {props.menu.map((menu)=><li  key={menu.id} >{ menu.title }</li>)}
-      </ul>
-    </section>
-  )
+export function GeneralComponent(){
+  return <h1>General Component</h1>
 }
 
-export function Footer(props){
-  return (
-    <section>
-      <p> Copyright {props.year}</p>
-    </section>
-  )
-}
+function App(props) {
+ return ( <div>
+    {props.authorized ? <SecretComponent /> : <GeneralComponent />}
 
-
-const menu=[
-  'Burger',
-  'Biff',
-  'Pasta'
-];
-
-
-const menuAsObj=menu.map((menu, i)=>({id:i, title: menu}))
-
-
-
-function App() {
-  return (
-    <div className="App">
-      <Header name="Deli Bar&Kök "/>
-      <Main menu={menuAsObj}/>
-      <Footer year={new Date().getFullYear()}/>
-    </div>
-  );
+  </div>
+ );
 }
 
 export default App;
