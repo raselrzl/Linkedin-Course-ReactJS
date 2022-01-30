343d0aa076451a8c8c5458bc12bcd8dbb0c78da4
Rasel Miah <rasel6041@gmail.com>
2022-01-30 03:55:15 +0100

    fetch data, view and handle

--- learning-linkdin/src/App.js
+++ learning-linkdin/src/App.js
@@ -3,6 +3,8 @@ import React, { useReducer, useState, useEffect } from 'react';
 
 function App({login}) {
   const [data, setData]=useState(null)
+  const[loading, setLoading]=useState(false)
+  const[error, setError]=useState(null)
 
 
 
@@ -10,20 +12,22 @@ function App({login}) {
     fetch(`https://api.github.com/users/${ login }`)
     .then((res)=>res.json())
     .then(setData)
-  },[]);
+    .then(()=>setLoading(false))
+    .catch(setError)
+  },[login]);
 
-  if(data){
-    return <div>
+  if(loading) return <h1>Loading...</h1>;
+  if(error)
+    return <pre>{JSON.stringify(error, null, 2)}</pre>
+
+  if(!data) return null;
+
+  
+    return (<div>
               <h1>{data.name}</h1>
               <img alt={data.login} src={data.avatar_url} />
           </div>
-  }
-
-        else return (
-          <div>
-              noUser available
-          </div>
- )
+     ) 
 }
 
 export default App;
